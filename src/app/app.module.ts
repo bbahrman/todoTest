c54b3437fa85248eb80ed8da1a58cab9774b2374
Ben Bahrman <benbahrman@gmail.com>
2017-07-21 06:01:03 -0700

    Developing from https://www.sitepoint.com/angular-2-tutorial/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
+
+// https://github.com/angular-buch/angular-cli-ghpages
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
